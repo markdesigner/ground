04204f4fea9735394d282d9a8e34cc767691a478
mark <mark.chang@vivotek.com>
2023-11-23 14:09:47 +0800

    chore: deploy to github pages

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VitePluginPages from 'vite-plugin-pages';
+import VitePluginVue from '@vitejs/plugin-vue';
+import VitePluginGhPages from 'vite-plugin-gh-pages';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    // VitePluginPages(),
+    // VitePluginVue(),
+    // VitePluginGhPages()
+  ],
+  base: '/techub/',
   css: {
     preprocessorOptions: {
       scss: {
